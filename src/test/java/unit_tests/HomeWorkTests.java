109dd1fa76ce9dfefc8348025c8cc5b14ca9823c
nikitashevtsov94 <156593109+nikitashevtsov94@users.noreply.github.com>
2025-03-16 23:12:02 +0300

    add first tests

--- src/test/java/unit_tests/HomeWorkTests.java
+++ src/test/java/unit_tests/HomeWorkTests.java
@@ -1,4 +1,35 @@
 package unit_tests;
 
+import application.Programms;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.math.BigInteger;
+
 public class HomeWorkTests {
+
+    @ParameterizedTest(name = "Проверка вычисления факториала числа {index} {0}")
+    @MethodSource(value = "dataProvider")
+    @DisplayName("Тест для проверки расчета факториала числа")
+    public void testFactorials(int result, int param) {
+        Assertions.assertEquals(BigInteger.valueOf(result), Programms.calculateFactorial(param),
+                String.format("Расчитанный факториал числа %d не соответствует ожиданиям", param));
+    }
+
+    @Test
+    @DisplayName("Тест для проверки расчета факториала числа")
+    public void testFactorials2() {
+        Assertions.assertThrows(ArithmeticException.class, () -> Programms.calculateFactorial(-1), "блабла");
+    }
+
+    private static Object[][] dataProvider() {
+        return new Object[][] {
+                {1, 1},
+                {1, 0},
+                {3, 4}
+        };
+    }
 }
