f5ff90624af289e721d19d5606146a0ef5e8a7c3
Nikita Shevtsov <shevtsov10089@gmail.com>
2025-03-18 13:56:00 +0300

    create first testng test

--- src/test/java/unit_tests/HomeWorkTests.java
+++ src/test/java/unit_tests/HomeWorkTests.java
@@ -1,12 +1,29 @@
 package unit_tests;
 
+import application.Factorial;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import org.testng.asserts.Assertion;
+import org.testng.Assert;
+
+import java.math.BigInteger;
+
+import static org.testng.Assert.assertEquals;
 
 public class HomeWorkTests {
-
-    @Test
-    void test() {
+    @DataProvider
+    private static Object[][] dataProviderForFactorial() {
+        return new Object[][]{
+                {1, 1},
+                {1, 0},
+                {24, 4},
+                {3628800, 10}
+        };
+    }
+    @Test(dataProvider = "dataProviderForFactorial")
+    public void testFactorialsPositive(int result, int param) {
+        assertEquals(BigInteger.valueOf(result), Factorial.calculateFactorial(param),
+                String.format("Расчитанный факториал числа %d не соответствует ожиданиям", param));
+        }
 
     }
-}
+
