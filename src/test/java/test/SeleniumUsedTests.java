c385bfc91dccc4743e9d7706e9a674224f10dfff
Nikita Shevtsov <shevtsov10089@gmail.com>
2025-03-19 12:04:26 +0300

    add selenium and webDriverManager dependencies in pom

--- src/test/java/test/SeleniumUsedTests.java
+++ src/test/java/test/SeleniumUsedTests.java
@@ -1,4 +1,29 @@
 package test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SeleniumUsedTests {
+    WebDriver driver;
+
+    @BeforeAll
+    static void setUpChromeDriver() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUpDriverObject() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearsDown() {
+        driver.quit();
+    }
+
+
 }
