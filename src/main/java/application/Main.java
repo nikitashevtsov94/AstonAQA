e034867a1e2cd56cab664fefcd42875f120fa130
nikitashevtsov94 <156593109+nikitashevtsov94@users.noreply.github.com>
2025-03-19 22:46:03 +0300

    add two tests

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -2,6 +2,7 @@ package application;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HI");
+
+        System.out.println();
     }
 }
--- src/test/java/test/SeleniumUsedTests.java
+++ src/test/java/test/SeleniumUsedTests.java
@@ -2,6 +2,8 @@ package test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -13,9 +15,14 @@ import java.time.Duration;
 class SeleniumUsedTests {
     WebDriver driver;
     WebDriverWait wait;
-    WebElement onlineReplenishmentWithoutCommissionForm;
-
-
+    By payFormWrapper = By.xpath("//div[contains(@class, 'pay__wrapper')]");
+    By formHeader = By.xpath("//h2[normalize-space(.)='Онлайн пополнение без комиссии']");
+    By cookieAgreeButtonLocator = By.xpath("//button[contains(@id,'cookie-agree')]");
+    By visaLogoLocator = By.xpath("//img[@alt='Visa']");
+    By verifiedByVisaLocator = By.xpath("//img[@alt='Verified By Visa']");
+    By masterCardLocator = By.xpath("//img[@alt='MasterCard'and@xpath='1']");
+    By masterCardSecureCodeLocator = By.xpath("//img[@alt='MasterCard Secure Code']");
+    By BelcardLocator= By.xpath("//img[@alt='Белкарт'and@xpath='1']");
     @BeforeAll
     static void setUpChromeDriver() {
         WebDriverManager.chromedriver().setup();
@@ -28,23 +35,48 @@ class SeleniumUsedTests {
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         try {
             driver.get("http://mts.by");
-            onlineReplenishmentWithoutCommissionForm = wait.until(ExpectedConditions.visibilityOfElementLocated(
-                    By.xpath("//div[contains(@class, 'pay__wrapper')]")));
-        } catch (Exception e) {
-            System.out.println("Форма не загрузилась" + e.getMessage());
+            WebElement cookieAgreeButton = driver.findElement(cookieAgreeButtonLocator);
+            cookieAgreeButton.click();
+            wait.until(ExpectedConditions.visibilityOfElementLocated(payFormWrapper));
+
+        } catch (NoSuchElementException e) {
+            System.out.println("Форма Cookie не появилась" + e.getMessage());
+        } catch (TimeoutException e) {
+            System.out.println("Страница не загрузилась" + e.getMessage());
         }
 
 
     }
-//    @AfterEach
-//    void tearsDown() {
-//        driver.quit();
-//    }
+
+    @AfterEach
+    void tearsDown() {
+        driver.quit();
+    }
 
     @Test
     @DisplayName("Тест на соответствие названия блока «Онлайн пополнение без комиссии»")
-    void test1() {
-    Assertions.assertEquals("Онлайн пополнение без комиссии",
-     onlineReplenishmentWithoutCommissionForm.       );
+    void OnlineReplenishmentWithoutCommissionHeaderTest() {
+        WebElement header = driver.findElement(formHeader);
+        Assertions.assertEquals("Онлайн пополнение без комиссии", header.getText().replace("\n", " "));
     }
+
+
+    @Test
+    void logoPaymentSystemPresenceTest() {
+        driver.findElement(visaLogoLocator);
+        Assertions.assertDoesNotThrow(() -> driver.findElement(visaLogoLocator));
+    }
+//    @ParameterizedTest
+//    @MethodSource(value = "dataProvider")
+//    @DisplayName("Тест наличия логотипов платежных систем")
+//    void logoPaymentSystemPresenceTest(String attributeValue) {
+//
+//    }
+//
+//    static Object[][] dataProvider() {
+//        return new Object[][] {
+//                {}
+//        }
+//    }
+
 }
