ea342a01358fd8b6a435b602a9050d16de6051d5
Nikita Shevtsov <shevtsov10089@gmail.com>
2025-03-20 13:49:57 +0300

    add third test

--- src/test/java/test/SeleniumUsedTests.java
+++ src/test/java/test/SeleniumUsedTests.java
@@ -10,6 +10,9 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 
 class SeleniumUsedTests {
@@ -18,11 +21,12 @@ class SeleniumUsedTests {
     By payFormWrapper = By.xpath("//div[contains(@class, 'pay__wrapper')]");
     By formHeader = By.xpath("//h2[normalize-space(.)='Онлайн пополнение без комиссии']");
     By cookieAgreeButtonLocator = By.xpath("//button[contains(@id,'cookie-agree')]");
-    By visaLogoLocator = By.xpath("//img[@alt='Visa']");
-    By verifiedByVisaLocator = By.xpath("//img[@alt='Verified By Visa']");
-    By masterCardLocator = By.xpath("//img[@alt='MasterCard'and@xpath='1']");
-    By masterCardSecureCodeLocator = By.xpath("//img[@alt='MasterCard Secure Code']");
-    By BelcardLocator= By.xpath("//img[@alt='Белкарт'and@xpath='1']");
+    static By visaLogoLocator = By.xpath("//img[@alt='Visa']");
+    static By verifiedByVisaLocator = By.xpath("//img[@alt='Verified By Visa']");
+    static By masterCardLocator = By.xpath("//img[@alt='MasterCard'and@xpath='1']");
+    static By masterCardSecureCodeLocator = By.xpath("//img[@alt='MasterCard Secure Code']");
+    static By belcardLocator = By.xpath("//img[@alt='Белкарт'and@xpath='1']");
+    //a[contains(text(),'Подробнее о сервисе')]
     @BeforeAll
     static void setUpChromeDriver() {
         WebDriverManager.chromedriver().setup();
@@ -31,16 +35,16 @@ class SeleniumUsedTests {
     @BeforeEach
     void setUpDriverObject() {
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        driver.get("https://www.mts.by/");
         try {
-            driver.get("http://mts.by");
-            WebElement cookieAgreeButton = driver.findElement(cookieAgreeButtonLocator);
+            WebElement cookieAgreeButton = wait.until(ExpectedConditions.elementToBeClickable((cookieAgreeButtonLocator)));
             cookieAgreeButton.click();
-            wait.until(ExpectedConditions.visibilityOfElementLocated(payFormWrapper));
-
-        } catch (NoSuchElementException e) {
+        } catch (TimeoutException e) {
             System.out.println("Форма Cookie не появилась" + e.getMessage());
+        }
+        try {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(payFormWrapper));
         } catch (TimeoutException e) {
             System.out.println("Страница не загрузилась" + e.getMessage());
         }
@@ -60,23 +64,35 @@ class SeleniumUsedTests {
         Assertions.assertEquals("Онлайн пополнение без комиссии", header.getText().replace("\n", " "));
     }
 
+    @ParameterizedTest
+    @MethodSource(value = "dataProvider" )
+    @DisplayName("Тест наличия логотипов платежных систем")
+    void logoPaymentSystemPresenceTest(By attributeValue) {
+        try {
+            boolean presence = wait.until(ExpectedConditions.visibilityOfElementLocated(attributeValue)) != null;
+            Assertions.assertTrue(presence, "Лого не появилось");
+        } catch (TimeoutException e) {
+            System.out.println("Отсутствует лого платежной системы: " + attributeValue);
+        }
+
+    }
+
+    static List<By> dataProvider() {
+        return Arrays.asList(visaLogoLocator, verifiedByVisaLocator, masterCardLocator,
+                masterCardSecureCodeLocator, belcardLocator);
+    }
 
     @Test
-    void logoPaymentSystemPresenceTest() {
-        driver.findElement(visaLogoLocator);
-        Assertions.assertDoesNotThrow(() -> driver.findElement(visaLogoLocator));
+    @DisplayName("Тест на проверку ссылки 'Подробнее о сервисе'")
+    void hyperTextTest() {
+        WebElement hyperText = wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//a[contains(text(),'Подробнее о сервисе')]")));
+        hyperText.click();
+        System.out.println(driver.getTitle());
+        WebElement header = driver.findElement(By.xpath("//meta[contains(@content, " +
+                "'Порядок оплаты и безопасность интернет платежей')]"));
+        String metaContent = header.getAttribute("content");
+        Assertions.assertEquals("Порядок оплаты и безопасность интернет платежей", metaContent);
     }
-//    @ParameterizedTest
-//    @MethodSource(value = "dataProvider")
-//    @DisplayName("Тест наличия логотипов платежных систем")
-//    void logoPaymentSystemPresenceTest(String attributeValue) {
-//
-//    }
-//
-//    static Object[][] dataProvider() {
-//        return new Object[][] {
-//                {}
-//        }
-//    }
 
 }
